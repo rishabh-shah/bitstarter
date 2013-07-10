a46e91050059205f9d89c7b12c0254b31c1e1520
Rishabh Shah <rishabh.shah0330@gmail.com>
2013-07-10 04:58:00 +0000

    Add new index.html file

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var webPageBuf = fs.readFileSync('index.html');
+  response.send(webPageBuf.toString());
 });
 
 var port = process.env.PORT || 5000;
